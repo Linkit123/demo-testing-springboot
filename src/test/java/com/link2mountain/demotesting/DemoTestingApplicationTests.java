5498252af29c76c2426f1f2c702d5c7e297e5985
link2mountain <linhtn@vnpay.vn>
2021-10-24 12:53:22 +0700

    init

---
+++ src/test/java/com/link2mountain/demotesting/DemoTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link2mountain.demotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
